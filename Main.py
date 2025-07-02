84784bab013982c600afe6d28649a1c6554d3aeb
Mravacardo <matthewheathcote@icloud.com>
2025-07-02 16:57:21 +0100

    Main.py

--- Main.py
+++ Main.py
@@ -89,3 +89,86 @@ cv2.imshow("Bird.png", img)
 # cv2.imshow('Grayscale Image', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
+import cv2
+
+img = cv2.imread('eagle .jpg')
+
+# start_point = (0, 50)
+
+# end_point = (100, 50)
+
+# colour = (235, 124, 243)
+
+# thickness = 15
+
+# cv2.imshow('original', img)
+
+# img1 = cv2.line(img, start_point, end_point, colour, thickness)
+
+# cv2.imshow('line image', img1)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# rectangle drawing
+
+# start_point = (100, 50)
+
+# end_point = (400, 450)
+
+# colour = (235, 124, 243)
+
+# thickness: - = filled, + = outline
+
+# thickness = -1
+
+# cv2.imshow('original', img)
+
+# img1 = cv2.rectangle(img, start_point, end_point, colour, thickness)
+
+# cv2.imshow('rectangle image', img1)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# circle drawing
+
+# centre_point = (300, 200)
+
+# radius = (60)
+
+# colour = (235, 124, 243)
+
+# thickness = -1
+
+# cv2.imshow('original', img)
+
+# img1 = cv2.circle(img, centre_point, radius, colour, thickness)
+
+# cv2.imshow('circle image', img1)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# text
+
+# origin = (130, 240)
+
+# font = cv2.FONT_HERSHEY_COMPLEX
+
+# font_scale = 1.5
+
+# colour = (231, 142, 132)
+
+# thickness = 5
+
+# cv2.imshow('original', img)
+
+# img1 = cv2.putText(img, "Hello World", origin, font, font_scale, colour, thickness, cv2.LINE_AA)
+
+# cv2.imshow('text image', img1)
+
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
